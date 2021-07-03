78159ba069550efeaa145e2fb9efcd5a0a1b1fe2
Zinkil-YT <74324305+Zinkil-YT@users.noreply.github.com>
2021-07-03 18:35:02 +0200

    Add files via upload

---
+++ src/Zinkil/AlwaysDay/Main.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+
+███████╗ ██╗ ███╗  ██╗ ██╗  ██╗ ██╗ ██╗
+╚════██║ ██║ ████╗ ██║ ██║ ██╔╝ ██║ ██║
+  ███╔═╝ ██║ ██╔██╗██║ █████═╝  ██║ ██║
+██╔══╝   ██║ ██║╚████║ ██╔═██╗  ██║ ██║
+███████╗ ██║ ██║ ╚███║ ██║ ╚██╗ ██║ ███████╗
+╚══════╝ ╚═╝ ╚═╝  ╚══╝ ╚═╝  ╚═╝ ╚═╝ ╚══════╝
+
+CopyRight : Zinkil-YT :)
+Github : https://github.com/Zinkil-YT
+Youtube : https://www.youtube.com/channel/UCW1PI028SEe2wi65w3FYCzg
+Discord Account : Zinkil#2006
+Discord Server : https://discord.gg/2zt7P5EUuN
+
+ */
+
+declare(strict_types=1);
+
+namespace Zinkil\AlwaysDay;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\math\Vector3;
+use pocketmine\network\mcpe\protocol\EventPacket;
+use pocketmine\network\mcpe\protocol\LevelEventPacket;
+use Zinkil\AlwaysDay\Tasks\SetDayTask;
+
+class Main extends PluginBase{
+
+    private static $instance;
+
+    public function onEnable(): void{
+
+        self::$instance = $this;
+
+        $this->setTasks();
+
+        $this->getLogger()->info("--- AlwaysDay V1.0.0 By Zinkil ---");
+    }
+
+    public static function getInstance(): Main{
+        return self::$instance;
+    }
+
+    public function setTasks(){
+        $this->getScheduler()->scheduleRepeatingTask(new SetDayTask($this), 20);
+    }
+}
---
+++ src/Zinkil/AlwaysDay/Tasks/SetDayTask.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+
+███████╗ ██╗ ███╗  ██╗ ██╗  ██╗ ██╗ ██╗
+╚════██║ ██║ ████╗ ██║ ██║ ██╔╝ ██║ ██║
+  ███╔═╝ ██║ ██╔██╗██║ █████═╝  ██║ ██║
+██╔══╝   ██║ ██║╚████║ ██╔═██╗  ██║ ██║
+███████╗ ██║ ██║ ╚███║ ██║ ╚██╗ ██║ ███████╗
+╚══════╝ ╚═╝ ╚═╝  ╚══╝ ╚═╝  ╚═╝ ╚═╝ ╚══════╝
+
+CopyRight : Zinkil-YT :)
+Github : https://github.com/Zinkil-YT
+Youtube : https://www.youtube.com/channel/UCW1PI028SEe2wi65w3FYCzg
+Discord Account : Zinkil#2006
+Discord Server : https://discord.gg/2zt7P5EUuN
+
+ */
+
+declare(strict_types=1);
+
+namespace Zinkil\AlwaysDay\Tasks;
+
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\scheduler\Task;
+use Zinkil\AlwaysDay\Main;
+
+class SetDayTask extends Task{
+	
+	public function __construct(Main $plugin){
+		$this->plugin=$plugin;
+	}
+	public function onRun(int $currentTick): void{
+		foreach($this->plugin->getServer()->getLevels() as $level){
+			$level->setTime(1000);
+			$level->stopTime();
+		}
+	}
+}
